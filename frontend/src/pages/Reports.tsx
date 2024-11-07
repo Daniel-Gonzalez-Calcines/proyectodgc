d5f14bcc9e3cc99deef6a8a2cd9c253dc798a804
Daniel-Gonzalez-Calcines <dgc2211gc@gmail.com>
2024-11-07 12:53:43 +0100

    redux

---
+++ frontend/src/pages/Reports.tsx
@@ -0,0 +1,14 @@
+import { Typography } from "@mui/material";
+
+function Reports() {
+
+    return (
+        <>
+            <Typography>
+                Página Reports de Daniel González Calcines
+            </Typography>
+        </>
+    )
+}
+
+export default Reports;
